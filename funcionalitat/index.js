6b4173038c72a50a094105a91682033a74702dcf
Arnau <arnaucarrasco@ginebro.cat>
2020-04-23 20:12:09 +0200

    Nou botó per canviar el contrast

--- funcionalitat/index.js
+++ funcionalitat/index.js
@@ -21,7 +21,7 @@ $(this).css('font-size', parseInt(textamanyP)*1.5+"px");
 
 }
 
-function textamanyDisminuir(params) {
+function textamanyDisminuir() {
 $("label").each(function( index ) {
 textamanyLlabel=$(this).css('font-size');
 $(this).css('font-size', parseInt(textamanyLlabel)/1.5+"px");
@@ -41,4 +41,49 @@ $("p").each(function( index ) {
 textamanyP=$(this).css('font-size');
 $(this).css('font-size', parseInt(textamanyP)/1.5+"px");
 });
+}
+
+
+var primerCanviar = true;
+function canviarContrast() {
+if (primerCanviar){
+    
+    $("p").css('color', 'white');
+
+    $("h1").css('color', 'white');
+
+    $("h2").css('color', 'white');
+
+    $("label").css('color', 'white');
+
+    $("body").css('background-color', 'black');
+
+    $("button").css('background-color', 'black');
+
+    $("button").css('color', 'white');
+
+    primerCanviar = false;
+
+}else{
+    $("p").css('color', 'black');
+
+    $("h1").css('color', 'black');
+
+    $("h2").css('color', 'black');
+
+    $("label").css('color', 'black');
+
+    $("body").css('background-color', 'white');
+
+    $("button").css('background-color', 'white');
+
+    $("button").css('color', 'black');
+
+    primerCanviar = true;
+
+}
+
+
+
+
 }
